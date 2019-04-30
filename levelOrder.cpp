bd2004fd6ca809cc1283a191f7e63d21ff04cca4
coolnut12138 <1426345106@qq.com>
2019-04-30 16:11:48 +0800

    add level's average and level's maxVal

--- levelOrder.cpp
+++ levelOrder.cpp
@@ -107,3 +107,92 @@ public:
 	}
 };
 
+//leetcode 637.二叉树的层平均值
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	vector<double> averageOfLevels(TreeNode* root) {
+		queue<TreeNode*> pNode;
+		vector<double> ave;
+
+		if (root){
+			pNode.push(root);
+		}
+
+		while (!pNode.empty()){
+			//记录每一层结点的个数
+			int len = pNode.size();
+			double sum = 0;
+			//每一层的结点的值求和，并将每一层的结点的左右结点压入队列
+			for (int i = 0; i < len; i++){
+				TreeNode *curNode = pNode.front();
+				pNode.pop();
+				sum += curNode->val;
+
+				if (curNode->left){
+					pNode.push(curNode->left);
+				}
+				if (curNode->right){
+					pNode.push(curNode->right);
+				}
+			}
+			//每一层求一次平均值并压入队列
+			ave.push_back(sum / len);
+		}
+		return ave;
+	}
+};
+
+//leetcode 515.在每个树行中找最大值
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	//思路同leetcode 637. 
+	vector<int> largestValues(TreeNode* root) {
+		queue<TreeNode*> q;
+		vector<int> maxVec;
+
+		if (root){
+			q.push(root);
+		}
+
+		while (!q.empty()){
+			int len = q.size();
+			int max = q.front()->val;
+			//在每一层遍历找到最大值，然后入队
+			for (int i = 0; i < len; i++){
+				TreeNode *curNode = q.front();
+				q.pop();
+				if (curNode->val > max){
+					max = curNode->val;
+				}
+
+				if (curNode->left){
+					q.push(curNode->left);
+				}
+
+				if (curNode->right){
+					q.push(curNode->right);
+				}
+			}
+			maxVec.push_back(max);
+		}
+		return maxVec;
+	}
+};

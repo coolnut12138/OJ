0c563fdaa368712a515980ce36930be9d1d671d4
coolnut12138 <1426345106@qq.com>
2019-06-08 20:17:35 +0800

    day18

--- OJ/day18.cc
+++ OJ/day18.cc
@@ -19,3 +19,48 @@ int main()
     }
     return 0;
 }
+
+
+//通配符匹配
+链接：https://www.nowcoder.com/questionTerminal/43072d50a6eb44d2a6c816a283b02036
+来源：牛客网
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isMatch(const char *str1, const char *str2)
+{
+    if(*str1 == '\0' && *str2 == '\0'){
+        return true;
+    }
+    if(*str1 == '\0' || *str2 == '\0'){
+        return false;
+    }
+    
+    if(*str1 == '?'){
+        //问号只匹配一个字符
+        return isMatch(str1 + 1, str2 + 1);
+    }
+    if(*str1 == '*'){
+        //星号匹配0个或多个字符
+        return (isMatch(str1 + 1, str2) || isMatch(str1 + 1, str2 + 1) || isMatch(str1, str2 + 1));
+    }
+    if(*str1 == *str2){
+        return isMatch(str1 + 1, str2 + 1);
+    }
+    return false;
+}
+
+int main()
+{
+    string str1, str2;
+    while(cin >> str1 >> str2){
+        if(isMatch(str1.c_str(), str2.c_str())){
+            cout << "true" << endl;
+        }else {
+            cout << "false" << endl;
+        }
+    }
+    return 0;
+}
